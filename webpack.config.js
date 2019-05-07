05c0cf43b99aea74000943b033ee32e02b6792e9
Matt Cassara <cassaram09@gmail.com>
2019-05-07 16:35:31 -0400

    adds webpack; builds ES5 output

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components|dist)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new webpack.IgnorePlugin(/quill/)]
+};
